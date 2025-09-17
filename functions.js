849417d6ec9c237532187a2255215e8085ef15e9
Yassef.dev <153541748+Wahabyas@users.noreply.github.com>
2025-09-16 23:22:07 -0700

    power

--- functions.js
+++ functions.js
@@ -22,3 +22,37 @@ console.log(calculateDifference(22, 5));
 console.log(calculateDifference(12, 1));
 console.log(calculateDifference(17, 9));
 
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(2, 5));
+console.log(calculateSum(10, 10));
+console.log(calculateSum(5, 5));
+
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));
+
+function calculateProduct(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(calculateProduct(13, 5));
+
+
+function calculateQuotient(num1, num2) {
+  return num2 === 0 ? "Error: Division by zero" : num1 / num2;
+}
+
+console.log(calculateQuotient(7, 11));
+console.log(calculateQuotient(3, 0));
+
+function calculateSquare(num) {
+  return num ** 2;
+}
+
