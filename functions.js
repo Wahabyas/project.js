81ba5c02d24dd87754d6306c5975ac784c52761b
Yassef.dev <153541748+Wahabyas@users.noreply.github.com>
2025-09-16 23:13:31 -0700

    return 2 + 7

--- functions.js
+++ functions.js
@@ -3,3 +3,22 @@ function addTwoAndSeven() {
 }
 
 console.log(addTwoAndSeven());
+
+
+
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(2, 5));
+console.log(calculateSum(10, 10));
+console.log(calculateSum(5, 5));
+
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));
+
